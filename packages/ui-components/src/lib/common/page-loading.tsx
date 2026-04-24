af63ac6098aa7537bd6127e358a44d0326b7203b
Dave Teply <davehamdan@gmail.com>
2026-04-24 15:08:08 +0000

    feat: translating loading messages

--- packages/ui-components/src/lib/common/page-loading.tsx
+++ packages/ui-components/src/lib/common/page-loading.tsx
@@ -1,8 +1,21 @@
+'use client';
+
+import { useTranslations } from 'next-intl';
+import { useEffect, useState } from 'react';
+
 export function PageLoading({ entityName }: { entityName: string }) {
+  const t = useTranslations('Common.Loading');
+  const [randomIndex, setRandomIndex] = useState(0);
+
+  useEffect(() => {
+    const count = parseInt(t('count'), 10);
+    setRandomIndex(Math.floor(Math.random() * count));
+  }, [t]);
+
   return (
     <div className="bg-base-100/50 absolute inset-0 z-50 flex items-center justify-center backdrop-blur-none">
       <div>
-        <span className="mr-2 capitalize">Loading {entityName}</span>
+        <span className="mr-2 capitalize">{t(randomIndex.toString(), { entityName })}</span>
         <span className="loading loading-bars loading-xs text-primary"></span>
       </div>
     </div>
