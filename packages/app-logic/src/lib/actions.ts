fe5c1798b3a54bf7f49812bfe889fe91c347c93d
Dave Teply <davehamdan@gmail.com>
2026-04-30 19:54:07 +0000

    feat: making event actions mobile friendly

--- packages/app-logic/src/lib/actions.ts
+++ packages/app-logic/src/lib/actions.ts
@@ -2,71 +2,6 @@ import { Action, DirectionType, SourceType } from '@job-tracker/domain';
 
 export const AVAILABLE_ACTIONS: Action[] = [
   {
-    id: 'applied-to-role',
-    nameKey: 'actionAppliedToRole',
-    iconName: 'DocumentPlusIcon',
-    defaults: {
-      eventTypeName: 'Applied',
-      direction: DirectionType.Outbound,
-      source: SourceType.Website,
-      suggestReminderDays: 5,
-    },
-  },
-  {
-    id: 'recruiter-outreach',
-    nameKey: 'actionRecruiterOutreach',
-    iconName: 'ChatBubbleLeftEllipsisIcon',
-    defaults: {
-      eventTypeName: 'Recruiter Outreach',
-      direction: DirectionType.Inbound,
-      source: SourceType.LinkedIn,
-    },
-  },
-  {
-    id: 'sent-follow-up',
-    nameKey: 'actionSentFollowUp',
-    iconName: 'PaperAirplaneIcon',
-    defaults: {
-      eventTypeName: 'Follow-up Sent',
-      direction: DirectionType.Outbound,
-      source: SourceType.Email,
-      suggestReminderDays: 5,
-    },
-  },
-  {
-    id: 'email-received',
-    nameKey: 'actionEmailReceived',
-    iconName: 'EnvelopeIcon',
-    defaults: {
-      eventTypeName: 'Email Received',
-      direction: DirectionType.Inbound,
-      source: SourceType.Email,
-    },
-  },
-  {
-    id: 'scheduled-interview',
-    nameKey: 'actionScheduledInterview',
-    iconName: 'CalendarIcon',
-    defaults: {
-      eventTypeName: 'Interview Scheduled',
-      direction: DirectionType.Inbound,
-      source: SourceType.Email,
-      suggestReminderDays: -1,
-      suggestReminderRelativeToEvent: true,
-    },
-  },
-  {
-    id: 'interview-completed',
-    nameKey: 'actionInterviewCompleted',
-    iconName: 'ClipboardDocumentCheckIcon',
-    defaults: {
-      eventTypeName: 'Interview Completed',
-      direction: DirectionType.Outbound,
-      source: SourceType.Website,
-      suggestReminderDays: 1,
-    },
-  },
-  {
     id: 'rejection-received',
     nameKey: 'actionRejectionReceived',
     iconName: 'XCircleIcon',
@@ -84,6 +19,71 @@ export const AVAILABLE_ACTIONS: Action[] = [
       eventTypeName: 'Networking/Coffee Chat',
       direction: DirectionType.Outbound,
       source: SourceType.LinkedIn,
+    },
+  },
+  {
+    id: 'interview-completed',
+    nameKey: 'actionInterviewCompleted',
+    iconName: 'ClipboardDocumentCheckIcon',
+    defaults: {
+      eventTypeName: 'Interview Completed',
+      direction: DirectionType.Outbound,
+      source: SourceType.Website,
+      suggestReminderDays: 1,
+    },
+  },
+  {
+    id: 'scheduled-interview',
+    nameKey: 'actionScheduledInterview',
+    iconName: 'CalendarIcon',
+    defaults: {
+      eventTypeName: 'Interview Scheduled',
+      direction: DirectionType.Inbound,
+      source: SourceType.Email,
+      suggestReminderDays: -1,
+      suggestReminderRelativeToEvent: true,
+    },
+  },
+  {
+    id: 'email-received',
+    nameKey: 'actionEmailReceived',
+    iconName: 'EnvelopeIcon',
+    defaults: {
+      eventTypeName: 'Email Received',
+      direction: DirectionType.Inbound,
+      source: SourceType.Email,
+    },
+  },
+  {
+    id: 'sent-follow-up',
+    nameKey: 'actionSentFollowUp',
+    iconName: 'PaperAirplaneIcon',
+    defaults: {
+      eventTypeName: 'Follow-up Sent',
+      direction: DirectionType.Outbound,
+      source: SourceType.Email,
+      suggestReminderDays: 5,
+    },
+  },
+  {
+    id: 'recruiter-outreach',
+    nameKey: 'actionRecruiterOutreach',
+    iconName: 'ChatBubbleLeftEllipsisIcon',
+    defaults: {
+      eventTypeName: 'Recruiter Outreach',
+      direction: DirectionType.Inbound,
+      source: SourceType.LinkedIn,
+    },
+  },
+  {
+    id: 'applied-to-role',
+    nameKey: 'actionAppliedToRole',
+    iconName: 'DocumentPlusIcon',
+    defaults: {
+      eventTypeName: 'Applied',
+      direction: DirectionType.Outbound,
+      source: SourceType.Website,
+      suggestReminderDays: 5,
     },
   },
 ];
