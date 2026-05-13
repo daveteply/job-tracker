06ae86b7c1d42621dd6ae8c6deb29714d249b252
Dave Teply <davehamdan@gmail.com>
2026-05-13 12:54:31 +0000

    fix: adding placeholder tests

---
+++ apps/web-ui/src/app/placeholder.spec.ts
@@ -0,0 +1,5 @@
+describe('web-ui placeholder', () => {
+  it('should pass', () => {
+    expect(true).toBe(true);
+  });
+});
---
+++ packages/domain/src/lib/placeholder.spec.ts
@@ -0,0 +1,5 @@
+describe('domain placeholder', () => {
+  it('should pass', () => {
+    expect(true).toBe(true);
+  });
+});
